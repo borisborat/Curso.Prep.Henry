1e861c62337b09abaac53bf8226e815359e847e7
borisborat <aboragini@gmail.com>
2022-10-19 19:02:51 -0300

    new homework update

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -19,12 +19,7 @@ function numberOfCharacters(string) {
   //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
-  //Escribe tu código aquí
-  let letterCounter = {}
-
-  for (const x in string) {
-    if (Object.hasOwnProperty.call(object, x))string const element = object[x]string    }
-  }
+  //Escribe tu código aqu
 }
 
 
