82fb1300d139d9f7cd77462840fcd302174cf09a
borisborat <aboragini@gmail.com>
2022-10-19 18:56:59 -0300

    Update homework

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,18 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1 , nombre.lenght)
 }
+
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -23,12 +26,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  return cb(numeros.reduce(function(acc, elemento){
+    return acc + elemento;
+}))
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  array.forEach(function(elemento) {
+    cb(elemento)
+});
 }
 
 function map(array, cb) {
@@ -36,12 +46,21 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const newArr = array.map(function (elemento){
+    return cb(elemento);
+  })
+  return newArr;
 }
 
 function filter(array) {
-  //Filtrar todos los elementos del array que comiencen con la letra "a".
-  //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
+  // Filtrar todos los elementos del array que comiencen con la letra "a".
+  // Devolver un nuevo array con los elementos que cumplen la condición
+  // Tu código:
+  let arrayFiltrado = array.filter(function(elemento){
+    return elemento.charAt(0) === 'a' || elemento.charAt(0) === 'A'
+  })
+  
+  return arrayFiltrado
 }
 
 // No modificar nada debajo de esta línea
@@ -56,3 +75,6 @@ module.exports = {
   map,
   filter
 };
+
+
+
--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,8 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  const nuevoObjeto = Object.entries(objeto)
+  return objeto
 }
 
 
@@ -18,6 +20,11 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let letterCounter = {}
+
+  for (const x in string) {
+    if (Object.hasOwnProperty.call(object, x))string const element = object[x]string    }
+  }
 }
 
 
